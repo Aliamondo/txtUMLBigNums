84dac9fdbe45a4fc3dc28046ec5d6c330bb4414d
Aliamondo <andra-92@yandex.ru>
2016-11-15 20:38:39 +0100

    Added remaining BigInteger methods and deleted static pseudo-constructors

--- src/math/BigInteger.java
+++ src/math/BigInteger.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Random;
+
 import hu.elte.txtuml.api.model.external.ExternalClass;
 
 public interface BigInteger extends ExternalClass {
@@ -7,16 +9,70 @@ public interface BigInteger extends ExternalClass {
 	public static final BigInteger ONE = new BigIntegerImplementation(java.math.BigInteger.ONE);
 	public static final BigInteger TEN = new BigIntegerImplementation(java.math.BigInteger.TEN);
 	
-	public static BigInteger BigInteger(byte[] val) {
-		return new BigIntegerImplementation(val);
-	}
-	public static BigInteger BigInteger(String val) {
-		return new BigIntegerImplementation(val);
+//	Static "constructor" as a workaround
+//	public static BigInteger BigInteger(String val) {
+//		return new BigIntegerImplementation(val);
+//	}
+	
+	public static BigInteger probablePrime(int bitLength, Random rnd) {
+		return new BigIntegerImplementation(java.math.BigInteger.probablePrime(bitLength, rnd));
 	}
 	
-	public BigInteger abs();
-	public String toString();
+	public static BigInteger probablePrime(int bitLength) {
+		// TODO Delete this or the previous method
+		return new BigIntegerImplementation(java.math.BigInteger.probablePrime(bitLength, new Random()));
+	}
+	
 	public static BigInteger valueOf(long val) {
 		return new BigIntegerImplementation(java.math.BigInteger.valueOf(val));
 	}
+	
+	public BigInteger abs();
+	public BigInteger add(BigInteger val);
+	public BigInteger and(BigInteger val);
+	public BigInteger andNot(BigInteger val);
+	public int bitCount();
+	public int bitLength();
+	public byte byteValue();
+	public byte byteValueExact();
+	public BigInteger clearBit(int n);
+	public int compareTo(BigInteger val);
+	public BigInteger divide(BigInteger val);
+	public BigInteger[] divideAndRemainder(BigInteger val);
+	public double doubleValue();
+	public boolean equals(Object x);
+	public BigInteger flipBit(int n);
+	public float floatValue();
+	public BigInteger gcd(BigInteger val);
+	public int getLowestSetBit();
+	public int hashCode();
+	public int intValue();
+	public int intValueExact();
+	public boolean isProbablePrime(int certainty);
+	public long longValue();
+	public long longValueExact();
+	public BigInteger max(BigInteger val);
+	public BigInteger min(BigInteger val);
+	public BigInteger mod(BigInteger m);
+	public BigInteger modInverse(BigInteger m);
+	public BigInteger modPow(BigInteger exponent, BigInteger m);
+	public BigInteger multiply(BigInteger val);
+	public BigInteger negate();
+	public BigInteger nextProbablePrime();
+	public BigInteger not();
+	public BigInteger or(BigInteger val);
+	public BigInteger pow(int exponent);
+	public BigInteger remainder(BigInteger val);
+	public BigInteger setBit(int n);
+	public BigInteger shiftLeft(int n);
+	public BigInteger shiftRight(int n);
+	public short shortValue();
+	public short shortValueExact();
+	public int signum();
+	public BigInteger subtract(BigInteger val);
+	public boolean testBit(int n);
+	public byte[] toByteArray();
+	public String toString();
+	public String toString(int radix);
+	public BigInteger xor(BigInteger val);
 }
--- src/math/Tester.java
+++ src/math/Tester.java
@@ -1,14 +1,21 @@
 package math;
 
+import java.util.Random;
+
 import hu.elte.txtuml.api.model.Action;
 import hu.elte.txtuml.api.model.execution.ModelExecutor;
-import math.model.Test;
+import math.model.TestBigInteger;
 
 public class Tester {
-	private static Test test;
+	private static TestBigInteger testBigInteger;
 	static void init() {
-		test = Action.create(Test.class);
-		Action.start(test);
+		testBigInteger = Action.create(TestBigInteger.class);
+		Action.start(testBigInteger);
+//		BigInteger[] arr = (new BigIntegerImplementation("42")).divideAndRemainder(new BigIntegerImplementation("15"));
+//		System.out.println(42 + " divideAndRemainder " + 15 + " = " + Arrays.toString(arr));
+		System.out.println("probablePrime(" + 10 + ") = " + BigInteger.probablePrime(10, new Random()));
+//		byte[] byteArr = new BigIntegerImplementation("42").toByteArray();
+//		System.out.println("42 toByteArray: " + Arrays.toString(byteArr));
 	}
 	public static void main(String[] args) {
 		ModelExecutor.create().setTraceLogging(true).start(Tester::init);
